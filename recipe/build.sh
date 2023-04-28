496d51e5f6604067d8ebc8faba480cad70a6be3f
Lori A. Burns <lori.burns@gmail.com>
2023-04-28 00:42:12 -0400

    try free blas for osx_arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,6 +12,12 @@ if [ "$(uname)" == "Linux" ]; then
     git rev-parse --show-toplevel
 fi
 
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    export lapack_libraries="${PREFIX}/lib/liblapack${SHLIB_EXT};${PREFIX}/lib/libblas${SHLIB_EXT}"
+else
+    export lapack_libraries="${PREFIX}/lib/libmkl_rt${SHLIB_EXT}"
+fi
+
 echo '__version_long = '"'$PSI4_PRETEND_VERSIONLONG'" > psi4/metadata.py
 
 # Note: bizarrely, Linux (but not Mac) using `-G Ninja` hangs on [205/1223] at
@@ -44,7 +50,7 @@ ${BUILD_PREFIX}/bin/cmake ${CMAKE_ARGS} ${ARCH_ARGS} \
   -D ENABLE_OPENMP=ON \
   -D ENABLE_XHOST=OFF \
   -D ENABLE_GENERIC=OFF \
-  -D LAPACK_LIBRARIES="${PREFIX}/lib/libmkl_rt${SHLIB_EXT}" \
+  -D LAPACK_LIBRARIES="${lapack_libraries}" \
   -D CMAKE_VERBOSE_MAKEFILE=OFF \
   -D CMAKE_PREFIX_PATH="${PREFIX}"
 
